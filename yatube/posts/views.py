39c4d627fc93a7fa7a5cbf2f2723e1c20f1d6b52
Gleb Kirsanov <dgeehi@gmail.com>
2022-10-13 09:25:24 +0300

    add first template

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render
 
 
 def index(request: HttpRequest):
-    return HttpResponse("Главная страница")
+    template = "posts/index.html"
+    return render(request, template)
 
 
 def group_posts(request: HttpRequest, slug: str):
